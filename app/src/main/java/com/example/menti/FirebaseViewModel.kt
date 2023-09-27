a239e94a0f76f8647aee05355ad6cd3d3fe7c0f2
artur <arturlauer1509@gmail.com>
2023-09-27 12:21:12 +0200

    Userprofil in Firestore anlegen und aktuallisieren

--- app/src/main/java/com/example/menti/FirebaseViewModel.kt
+++ app/src/main/java/com/example/menti/FirebaseViewModel.kt
@@ -1,21 +1,29 @@
 package com.example.menti
 
 import android.app.Application
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.menti.R
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.example.menti.ui.ProfileFragment
+import com.example.menti.ui.ProfileFragmentDirections
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import kotlin.coroutines.coroutineContext
 
 
 // val app hinzugefügt
 class FirebaseViewModel(val app: Application) : AndroidViewModel(app) {
 
-    val firebaseAuth = FirebaseAuth.getInstance()
+    val auth = FirebaseAuth.getInstance()
+    val firestore = FirebaseFirestore.getInstance()
+
+    //User Profil Dokument Referenz
+    lateinit var profileRef : DocumentReference
 
     private val _user: MutableLiveData<FirebaseUser?> = MutableLiveData()
     val user: LiveData<FirebaseUser?>
@@ -26,36 +34,22 @@ class FirebaseViewModel(val app: Application) : AndroidViewModel(app) {
     init {
         setupUserEnv()
     }
+    fun setupUserEnv(){
+        _user.value = auth.currentUser
 
-    //Google SignIn
-    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken(app.getString(R.string.web_client_id))
-        .requestEmail()
-        .build()
+        auth.currentUser?.let {
 
-    private val googleSignInClient = GoogleSignIn.getClient(app, gso)
-
-    fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                _user.value = firebaseAuth.currentUser
-            } else {
-                _user.value = null
-            }
+            // Profil Dokument Referenz aktuallisieren == CurrentUser
+            profileRef = firestore.collection("Profile").document(auth.currentUser!!.email!!)
         }
     }
 
-    ///////
+    // -------------------------------------- E-MAIL & PASSWORT ------------------------------------------------- //
 
-    fun setupUserEnv(){
-        _user.value = firebaseAuth.currentUser
-    }
-
-    // Einloggen
+    //Einloggen
     fun signIn(email: String, password: String) {
 
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
             //Wenn Task fertig ist dann überprüfe z.B. ob der User eingeloggt wurde
             //oder ob es Fehler gab o.Ä.
 
@@ -63,21 +57,65 @@ class FirebaseViewModel(val app: Application) : AndroidViewModel(app) {
         }
     }
 
-    // Neuen Account erstellen
+    //Neuen Account erstellen
     fun signUp(email: String, password: String) {
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
             //Wenn Task fertig ist dann überprüfe z.B. ob der User eingeloggt wurde
             //oder ob es Fehler gab o.Ä.
 
             setupUserEnv()
+
+            // Nutzerprofil Daten
+            val userProfile = hashMapOf(
+                "anrede" to "",
+                "vorname" to "",
+                "name" to "",
+                "plz" to "",
+                "ort" to "",
+                "anschrift" to "",
+                "telefonnummer" to "",
+                "istEndnutzer" to true
+            )
+
+            // Daten in firestore Speichern
+            firestore.collection("Profile").document(email).set(userProfile)
         }
 
     }
 
     // Ausloggen
     fun signOut(){
-        firebaseAuth.signOut()
-        _user.value = firebaseAuth.currentUser
+        auth.signOut()
+        _user.value = auth.currentUser
     }
+
+    // -------------------------------------- FIRESTORE DATABASE ------------------------------------------------- //
+
+    //Nutzerdaten bearbeiten
+    fun editProfile(anrede: String, vorname: String, name: String, plz: String, ort: String, anschrift: String, telefonNummer: String, fragment: Fragment) {
+
+        val userProfileUpdate = hashMapOf(
+            "anrede" to anrede,
+            "vorname" to vorname,
+            "name" to name,
+            "plz" to plz,
+            "ort" to ort,
+            "anschrift" to anschrift,
+            "telefonnummer" to telefonNummer,
+            "istEndnutzer" to true
+        )
+
+        // Überschreibe Daten
+        firestore.collection("Profile").document(_user.value!!.email!!)
+            .set(userProfileUpdate, SetOptions.merge())
+            .addOnSuccessListener {
+                Toast.makeText(fragment.requireContext(), "Aktualisierung erfolgreich", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(fragment.requireContext(), "Fehler: " + e.message, Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
 }
--- app/src/main/java/com/example/menti/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/menti/ui/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.menti.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.menti.FirebaseViewModel
 import com.example.menti.R
 import com.example.menti.databinding.FragmentProfileBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,6 +16,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    val firebaseViewModel: FirebaseViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,10 +34,40 @@ class ProfileFragment : Fragment() {
         val navBar = requireActivity().findViewById<BottomNavigationView>(com.example.menti.R.id.bottomNavigation)
         navBar.visibility = View.GONE
 
+        //Zurück zum Home Fragment
         binding.backToHomeBTN.setOnClickListener {
 
             findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToHomeFragment())
             navBar.visibility = View.VISIBLE
+        }
+        // Profilinfo Anzeigen
+        firebaseViewModel.profileRef.addSnapshotListener { value, error ->
+            Log.d("Firebase", "$value , ${value?.data}")
+            value?.let {
+                binding.profileAnredeET.setText(value["anrede"].toString())
+                binding.profileVornameET.setText(value["vorname"].toString())
+                binding.profileNameET.setText(value["name"].toString())
+                binding.profilePlzET.setText(value["plz"].toString())
+                binding.profileOrtET.setText(value["ort"].toString())
+                binding.profileAnschriftET.setText(value["anschrift"].toString())
+                binding.profileTelefonnummerET.setText(value["telefonnummer"].toString())
+            }
+
+        }
+
+        //Profilangaben bearbeiten
+        binding.saveChangesBTN.setOnClickListener {
+
+            val anrede = binding.profileAnredeET.text.toString()
+            val vorname = binding.profileVornameET.text.toString()
+            val name = binding.profileNameET.text.toString()
+            val plz = binding.profilePlzET.text.toString()
+            val ort = binding.profileOrtET.text.toString()
+            val anschrift = binding.profileAnschriftET.text.toString()
+            val telefonNummer = binding.profileTelefonnummerET.text.toString()
+
+            firebaseViewModel.editProfile(anrede, vorname, name, plz, ort, anschrift, telefonNummer, this)
+
         }
     }
 
